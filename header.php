4e5dd66e536e48d0f59ab71d2323d972777495d2
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-02-07 15:12:28 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -7,9 +7,9 @@
   <title>StarBooks</title>
 </head>
 <header>
+  <a class=bouton href="#"><img src="panier.png" widht="20" height="20" align=middle>Mon panier</a>
   <div class=logo>
   <img src="logo" alt="logo StarBooks" align=left>
-
 </div>
   <?php require "nav.php"; ?>
 </header>
