f9b757012a1623ece200ea9d75f77de5f8539e09
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-02-07 14:51:35 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" type="image/png" href="logo.png">
+  <title>StarBooks</title>
+</head>
+<header>
+  <div class=logo>
+  <img src="logo" alt="logo StarBooks" align=left>
+
+</div>
+  <?php require "nav.php"; ?>
+</header>
+<body>
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,16 @@
+
+
+<nav>
+    <ul>
+      <li><a href="index.php">Menu</a></li>
+      <li class="deroulant"><a href="#">Trier par :</a>
+        <ul class="sous">
+          <li class="pow"><a href="#">Genre</a></li>
+          <li class="magie"><a href="#">Mes favoris</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Auteurs</a></li>
+      <li><a href="#">Mon compte</a></li>
+      <li><a href="#">Nous Contacter</a></li>
+    </ul>
+  </nav>
