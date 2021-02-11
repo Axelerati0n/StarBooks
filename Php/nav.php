120558d746ec81b89669a40863671df5c2963644
Axelerati0n <76069490+Axelerati0n@users.noreply.github.com>
2021-02-11 18:36:09 +0100

    Update nav.php

--- Php/nav.php
+++ Php/nav.php
@@ -7,12 +7,12 @@
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/auteurs.php">Auteurs</a></li>
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/compte.php">Mon compte</a></li>
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/contact.php">Nous Contacter</a></li>
-      <!--<li> A METTRE PLUS TARD DANS LA OU YA LES BGS DE LIVRES-->
+      <!--<li><php/* A METTRE PLUS TARD DANS LA OU YA LES BGS DE LIVRES
       <form name="form" action="" method="post">
-		<input type="text" name="search" id="search"> <!-- barre de recherche-->
+		<input type="text" name="search" id="search">  barre de recherche
 		<p><input type="submit" value="GO">
-	<!--	if (empty($_POST['search'])) {$search=NULL;
+		if (empty($_POST['search'])) {$search=NULL;
    }else {$search = htmlspecialchars($_POST['search']);// recuperer ce que  inscrit dans barre recherche
-	} */?></p></li>-->
+	} ?></p></li>-->
     </ul>
   </nav>
