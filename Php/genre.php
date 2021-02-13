695d34fcec99112c08011a79b6221f446cf58f2e
Axelerati0n <76069490+Axelerati0n@users.noreply.github.com>
2021-02-13 17:15:04 +0100

    Add files via upload

---
+++ Php/genre.php
@@ -0,0 +1,21 @@
+<section class="testt">
+        <h2 class="yoyo">Classement par genre</h2>
+            <div class="grid">
+                <ul class="bonsoir">
+                    <a href="tri.php"><li class="aventure">Aventure</li></a>
+                    <a href="tri.php"><li class="bd">Bande-Dessinée</li></a>                   
+                </ul>
+                <ul class="bonsoir">
+                    <a href="tri.php"><li class="fantastique">Fantastique</li></a>
+                    <a href="tri.php"><li class="horreur">Horreur</li></a>
+                </ul>
+                <ul class="bonsoir">
+                    <a href="tri.php"><li class="manga">Manga</li></a>
+                    <a href="tri.php"><li class="medieval">Médiéval</li></a>
+                </ul>
+                <ul class="bonsoir">
+                    <a href="tri.php"><li class="mythologie">Mythologie</li></a>
+                    <a href="tri.php"><li class="science">Science-Fiction</li></a>
+                </ul>
+            </div>
+    </section>
