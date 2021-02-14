f0125185ced2b5679d9c669191ba51ac0ed53534
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-02-14 16:00:31 +0100

    Add files via upload

--- Php/detail.php
+++ Php/detail.php
@@ -1,37 +1,73 @@
-<?php require "config.php";
-	  require "header.php";
- $isbn = $_GET['isbn'];
- $link = mysqli_connect($HOST, $user, $password, $database);
- if(!$link){
-	exit;
-   }
-if(!mysqli_set_charset($link,"utf8mb4")){
-	printf(mysqli_error($link));
-	exit();
-}
-$req = " SELECT titre,isbn FROM livre JOIN editeur e ON e.id=livre.editeur JOIN auteur a ON a.idLivre=livre.isbn JOIN personne p ON p.id=livre.editeur WHERE isbn = '$isbn';";
-$result = mysqli_query($link,$req);
-if ($result) {
-		while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-			echo "<a href='" . $CONFIG['root_path'] . "bibliotheque.php'><div><img src='" . $CONFIG['root_path'] . "img\Image1.jpg' id='logo'></a>";
-			echo "<h1 class='Titre'><font color='white'>" . $row['titre'] . "</h1></div>";
-			echo "<ul>";
-				echo "<div class='cali'>";
-					echo "<div><img src='" . $CONFIG['root_path'] . "img/" . $row["isbn"] . ".jpg'  alt='' class='L'><div align='center'>";
-						echo "<p class='pc'>Titre : " . $row["titre"] . "</p>";
-			  			echo "<p class='pc'>Auteur : " . $row["prenom"] . " " . $row["nom"] . "</p>";
-			    		echo "<p class='pc'>Editeur : " . $row["libelle"] . "</p>";
-			    		echo "<p class='pc'>Date de parution: " . $row["annee"] . "</p>";
-						echo "<p class='pc'>Isbn : " . $row["isbn"] . "</p>";
-						echo "<p class='pc'>Nombre de page : " . $row["nbpages"] . "</p>";
-						echo "<p class='pc'> " . $row["resume"] . "</p>";
-					echo "</div></div>";
-				echo "</div>";	
-			echo "</ul>";
-			echo "</div>";	
-		}
-		}
- ?>
-</div>
-<?php require"footer.php"?>
+<?php $page_title="secondaire";
+require "header.php";
+?>
+<?php require "config.php";?>
 
+
+  <section>
+    <?php
+
+        $link = mysqli_connect("localhost", "root", "", "StarBooks");
+        if(!mysqli_set_charset($link,"utf8mb4")){
+            printf("erreur lors du chargement du jeu de caractere utf8mb4 : %s\n", mysqli_error($link));
+            exit();
+        }
+        if(isset($_GET["isbn"])){
+           $get = $_GET["isbn"];
+
+
+
+
+           $req = "SELECT * FROM livre JOIN editeur  ON editeur.id=livre.editeur
+           JOIN auteur ON auteur.idLivre=livre.isbn JOIN personne ON personne.id=auteur.idPersonne JOIN genre ON genre.id=livre.genre WHERE isbn = $get;";;
+
+          $result = mysqli_query($link,$req);
+
+
+
+          if($result){
+              while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+
+            $isbn = $row["isbn"];
+            $titre = $row["titre"];
+
+            ?>
+
+
+
+          <h1><?php echo $titre ?></h1>
+
+            <div><img src=<?php echo "../img/Livres/{$isbn}.jpg"?> alt="<?php echo $isbn?>"></div>
+
+
+
+            <ul>
+              <?php
+                echo "<li><strong>Nom : </strong>" . $row["nom"] . "</li>";
+                echo "<li><strong>Prénom : </strong>" . $row["prenom"] . "</li>";
+                echo "<li><strong>éditeur : </strong>" . $row["nomediteur"] . "</li>";
+                echo "<li><strong>isbn : </strong>" . $row["isbn"] . "</li>";
+                echo "<li><strong>année de publication : </strong>" . $row["annee"] . "</li>";
+                echo "<li><strong>genre : </strong>" . $row["nomgenre"] . "</li>";
+                echo "<li><strong>Nombre de pages : </strong>" . $row["nbpages"] . "</li>";
+                echo"</ul>";
+                echo"<section class=info>";
+
+
+
+          }
+          mysqli_free_result($result);
+        }
+        mysqli_close($link);
+      }
+
+
+          ?>
+<?php require "footer.php";?>
+
+
+
+
+
+   </body>
+</html>
