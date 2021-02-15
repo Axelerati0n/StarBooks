db9c0fd3cbce95165faa26bc8314fcebf6dec6f5
Axelerati0n <76069490+Axelerati0n@users.noreply.github.com>
2021-02-15 09:52:25 +0100

    Update footer.php

--- Php/footer.php
+++ Php/footer.php
@@ -4,13 +4,13 @@
       <a href="#top"><p id="scroll_to_top">Haut de page</p></a>
     </div>
         <div class="bande">
-            <div class="icone"><img src="../img/camion.png" width="100px" height="100px">
+            <div class="icone"><img src="<?php echo $CONFIG["root_path"] ?>img/camion.png" width="100px" height="100px">
             <h4><strong>Une livraison rapide et gratuite</strong></h4><p>sans minimum d'achat</p></div>
-            <div class="icone"><img src="../img/cb.png" width="100px" height="100px">
+            <div class="icone"><img src="<?php echo $CONFIG["root_path"] ?>img/cb.png" width="100px" height="100px">
             <h4><strong>Possibilité de payer en plusieurs fois</strong></h4><p>En 2 à 3 fois</p></div>
-            <div class="icone"><img src="../img/30j.png" width="100px" height="100px">
+            <div class="icone"><img src="<?php echo $CONFIG["root_path"] ?>img/30j.png" width="100px" height="100px">
             <h4><strong>Satisfait ou remboursé</strong></h4><p>30 jours d'essai avant le renvoi</p></div>
-            <div class="icone"><img src="../img/service.png" width="100px" height="100px">
+            <div class="icone"><img src="<?php echo $CONFIG["root_path"] ?>img/service.png" width="100px" height="100px">
             <h4><strong>Un service client plus que présent</strong></h4><p>Disponible 24h/24</p></div>
 
         </div>
