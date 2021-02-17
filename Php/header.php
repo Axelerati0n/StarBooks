b836153f18edd4977d432de5609e36943256a12a
xtraide <75263739+xtraide@users.noreply.github.com>
2021-02-17 10:23:49 +0100

    Update header.php

--- Php/header.php
+++ Php/header.php
@@ -1,41 +1,35 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <?php require "Config.php"?>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="<?php echo $CONFIG["root_path"] ?>Css/style.css">
-  <link rel="shortcut icon" type="image/png" href="<?php echo $CONFIG["root_path"] ?>img/logo.png">
+  <link rel="stylesheet" href="../Css/style.css">
+  <link rel="shortcut icon" type="image/png" href="../img/logo.png">
+  <?php require "Config.php"?>
   <title>StarBooks</title>
 </head>
-
-
 <body>
 <div class=navigation>
-
-		
-<header>
-
-  <a class=bouton href="#"><img src="<?php echo $CONFIG["root_path"] ?>img/panier.png" widht="20" height="20" align=middle>Mon panier</a>
-  <div class=logo>
-  <img src="<?php echo $CONFIG["root_path"] ?>img/logo" alt="logo StarBooks" align=left>
-  <?php
+	<header>
+		<div class=logo>
+			<img src="../img/logo" alt="logo StarBooks" align=left>
+			<?php
 			session_start();
 			if (isset($_SESSION['login'])) {?>   <!-- si  login n'est pas null allor la persoone est connecter-->
 				<a class=bouton href="#"><img src="../img/panier.png" widht="20" height="20" align=middle>Mes réservations</a>
 				<a class=bouton href="deco.php"><img src="../img/compte.png" widht="20" height="20" align=middle>Deconnexion</a>
 				<a class=bouton href="membre.php"><img src="../img/compte.png" widht="20" height="20" align=middle>Espace membre</a><?php
+
 				// si le login est admin alors il s'ajit de l'admin
 					if($_SESSION['login'] == "admin"){?>
-						<a class=bouton href="admin.php"><img src="<?php echo $CONFIG["root_path"] ?>img/compte.png" widht="20" height="20" align=middle>Admin</a> <?php
+						<a class=bouton href="admin.php"><img src="../img/compte.png" widht="20" height="20" align=middle>Admin</a> <?php
 					}
 			}else{?><!--  si le login  est null alors --> 
-				<a class=bouton href="connexion.php"><img src="<?php echo $CONFIG["root_path"] ?>img/compte.png" widht="20" height="20" align=middle>Connexion</a><?php
+				<a class=bouton href="connexion.php"><img src="../img/compte.png" widht="20" height="20" align=middle>Connexion</a><?php
 			}?>
-  
+		</div>
+		<?php require "nav.php"; ?>
+	</header>
 </div>
+</body>
+</html>
 
-  <?php require "nav.php"; ?>
-
-</header>
-</div>
-<div class="marge"></div>
