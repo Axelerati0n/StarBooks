e32a55e018f40f87caf18265033826c11b3e88ef
xtraide <75263739+xtraide@users.noreply.github.com>
2021-02-17 22:18:57 +0100

    Update admin.php

--- Php/admin.php
+++ Php/admin.php
@@ -5,138 +5,137 @@
 </header>
 <form name="form" action="" method="post">
 <div class='infos'>
+echo "suprimer un Compte";
+?><form name="form" action="" method="post">
+			<p><input type="text" name="set1" placeholder="entre un mot de passe">
+			<input type="submit" value="GO"></p></form><?php
+			if (empty($_POST['set1'])) {$set1=NULL;
+		}else {$set1 = htmlspecialchars(md5($_POST['set1']));// recuperer ce que  inscrit dans barre recherche
+		}
+		$sql = " DELETE FROM membre WHERE mdp='$set1';"; 
+		//$sql = " DELETE FROM membre WHERE login='$text';"; // requete pour trier par ordre decroisant 
 
 
-       
-       	<input  name="trie" type="submit" value="supLI">
-       	<input  name="trie" type="submit" value="modifLI">  <br><br>
-       	<?php
+		echo "ajouter un  livre";?>
+			<form name="test" action="" method="post">
+				<p><input type="text" name="set2"  placeholder="Entrée un nbpage">
+				<input type="text" name="set1"  placeholder="Entrée un idlangue">
+				<input type="text" name="set3"  placeholder="Entrée un idEditeur">
+				<input type="text" name="set4"  placeholder="Entrée un titre">
+				<input type="text" name="set5"  placeholder="Entrée un ISBN">
+				<input type="text" name="set6"  placeholder="Entrée un annee">
+				<input type="text" name="set7"  placeholder="Entrée un idgenre">
+				<input type="text" name="set12"  placeholder="Entrée un resumer">
+				<input type="text" name="set8"  placeholder="Entrée un annee">
+				<input type="text" name="set9"  placeholder="Entrée un idPersone">
+				<input type="text" name="set10"  placeholder="Entrée un Empruntlivre">
+				<input type="text" name="set11"  placeholder="Entrée un IdGenre">
+				<input type="text" name="set13"  placeholder="Entrée un Idmembre">
+				<input type="submit" value="GO"></p>
+		</form>
+		<?php	
+				if (empty($_POST['set1'])) {$set1=NULL;
+		}else {$set1 = htmlspecialchars($_POST['set1']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set2'])) {$set1=NULL;
+		}else {$set2 = htmlspecialchars($_POST['set2']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set3'])) {$set3=NULL;
+		}else {$set3 = htmlspecialchars($_POST['set3']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set4'])) {$set4=NULL;
+		}else {$set4 = htmlspecialchars($_POST['set4']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set5'])) {$set5=NULL;
+		}else {$set5 = htmlspecialchars($_POST['set5']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set6'])) {$set6=NULL;
+		}else {$set6 = htmlspecialchars($_POST['set6']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set7'])) {$set7=NULL;
+		}else {$set7 = htmlspecialchars($_POST['set7']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set8'])) {$set8=NULL;
+		}else {$set8= htmlspecialchars($_POST['set8']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set9'])) {$set9=NULL;
+		}else {$set9 = htmlspecialchars($_POST['set9']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set10'])) {$set10=NULL;
+		}else {$set10 = htmlspecialchars($_POST['set10']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set11'])) {$set11=NULL;
+		}else {$set11 = htmlspecialchars($_POST['set11']);// recuperer ce que  inscrit dans barre recherche
+		}
+if (empty($_POST['set12'])) {$set12=NULL;
+		}else {$set12 = htmlspecialchars($_POST['set12']);// recuperer ce que  inscrit dans barre recherche
+		}
+		if (empty($_POST['set13'])) {$set13=NULL;
+		}else {$set13= htmlspecialchars($_POST['set13']);// recuperer ce que  inscrit dans barre recherche
+		}
 
-if (empty($_POST['trie'])) {$tri=NULL;
-}else {$tri = htmlspecialchars($_POST['trie']);// recuperer ce que  inscrit dans barre recherche
-} 
-       	switch ($tri) {
-	case 'modifLI';//modLI
-		echo "modifLI";?>
-		<form name="form" action="" method="post">
-	   		<input  name="tri2" type="submit" value="nbpage">
-       		<input  name="tri2" type="submit" value="idlangue">
-       		<input  name="tri2" type="submit" value="idEditeur">
-       		<input  name="tri2" type="submit" value="titre">
-       		<input  name="tri2" type="submit" value="isbn">
-       	</form>
-       	<form name="form" action="" method="post">
-			<p><input type="text" name="search" id="search" placeholder="Entrée un ISBN">
-			<input type="submit" value="GO"></p>
-		</form><?php
-	break;
-	case 'supLI'://sup livre
-	echo "supLI";
+		$sql ="INSERT INTO livre (Isbn, Titre, Annee, Nbpages, Resume, EmpruntLivre, IdPersonne, IdEditeur, IdGenre, IdLangue, idmembre) VALUES ('$set5', '$set4', '$set8', '$set1', '$set12', '$set10', '$set9', '$set3', '$set11', '$set1', '$set13')";
+
+	echo "suprimer LIVRE";
 	?><form name="form" action="" method="post">
-		<p><input type="text" name="search" id="search" placeholder="Entrée un ISBN">
-		<input type="submit" value="GO"></p>
-	</form><?php
-	//$sql = "DELETE FROM livre WHERE isbn='$text';"; // requete pour tier par ordre  croisant des titre
-	break;
-}
+	<p><input type="text" name="set1"  placeholder="Entrée un isbn">
+			<input type="submit" value="GO"></p></form><?php
+			if (empty($_POST['set1'])) {$set1=NULL;
+		}else {$set1 = htmlspecialchars($_POST['set1']);// recuperer ce que  inscrit dans barre recherche
+		}
+	$sql = "DELETE FROM livre WHERE isbn='$set1';"; 
+
+
 if (empty($_POST['tri2'])) {$tri2=NULL;
 		}else {$tri2 = htmlspecialchars($_POST['tri2']);// recuperer ce que  inscrit dans barre recherche
 		}
-switch ($tri2) {
-				case 'mdp':
-				echo "mdp";
-				?><p><input type="text" name="search" id="search" placeholder="Entrée un mot de passe ">
-			<input type="submit" value="GO"></p><?php
-				break;
-			case 'login':
-				echo $tri2;
-				?><p><input type="text" name="search" id="search" placeholder="Entrée un login">
-			<input type="submit" value="GO"></p><?php
-				break;
-			case 'isbn':
-				echo "isbn";
-			//$sql ="UPDATE livre SET isbn = '$search',  WHERE CustomerID = 1;";
-				break;
-			case 'idlangue':
-				echo "idlangue";
-				?><p><input type="text" name="search" id="search" placeholder="Entrée un idlangue">
-			<input type="submit" value="GO"></p><?php
-				break;
-			case 'idEditeur':
-				echo "idEditeur";
-				?><p><input type="text" name="search" id="search" placeholder="Entrée un idEditeur">
-			<input type="submit" value="GO"></p><?php
-				break;
-			case 'annee':
-				echo "annee";
-				?><p><input type="text" name="search" id="search" placeholder="Entrée un annee">
-			<input type="submit" value="GO"></p><?php
-				break;
-			case 'titre':
-				echo "titre";
-				?><p><input type="text" name="search" id="search" placeholder="Entrée un titre">
-			<input type="submit" value="GO"></p><?php
-				break;
-			case 'nbpage':
-				echo "nbpage";
-				?><p><input type="text" name="search" id="search" placeholder="Entrée un nbpage">
-			<input type="submit" value="GO"></p><?php
-				break;
+
+				echo " changer le mdp ";
+			?><form name="form" action="" method="post">
+					<p><input type="text" name="set1"  placeholder="ancien mot de passe ">
+						<input type="text" name="set2"  placeholder="nouveaux mot de passe">
+			<input type="submit" value="GO"></p></form><?php
+			if (empty($_POST['set1'])) {$set1=NULL;
+		}else {$set1 = htmlspecialchars(md5($_POST['set1']));// recuperer ce que  inscrit dans barre recherche
 		}
- $sql =" SELECT * FROM livre";
+		if (empty($_POST['set2'])) {$set2=NULL;
+		}else {$set2 = htmlspecialchars(md5($_POST['set2']));// recuperer ce que  inscrit dans barre recherche
+		}		
+ 		$sql = "UPDATE membre SET mdp ='$set2' WHERE mdp='$set1';";
+
+
+				echo "changer le login";
+				?><form name="form" action="" method="post">
+					<p><input type="text" name="set1" placeholder="ancien login">
+						<input type="text" name="set2"  placeholder="nouveaux mot de login">
+			<input type="submit" value="GO"></p></form><?php
+						if (empty($_POST['set1'])) {$set1=NULL;
+		}else {$set1 = htmlspecialchars($_POST['set1']);// recuperer ce que  inscrit dans barre recherche
+		}
+		if (empty($_POST['set2'])) {$set2=NULL;
+		}else {$set2 = htmlspecialchars($_POST['set2']);// recuperer ce que  inscrit dans barre recherche
+		}	
+			$sql="UPDATE membre SET  login='$set2' WHERE login='$set1';";
+
+
+		
+	echo $sql;
+require "config.php";
+$link = mysqli_connect($HOST , $user, $password, $database);
+if ((isset($_POST['isbn']) && !empty($_POST['isbn'])))
 	$link = mysqli_connect($HOST , $user, $password, $database);
-		$req = mysqli_query($link,$sql) or exit('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error());
+		$req = mysqli_query($link,$sql) or exit('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error($link));
 		$data = mysqli_fetch_array($req);
 		mysqli_free_result($req);
 		mysqli_close ($link);	
-
-https://www.topachat.com/pages/configomatic.php?c=i%2F%2FAasq7m%2Bq%2FigvGPg1JXVoTZc2iKxsva7HZDETMT%2BE%3D
-
 		if ($data[0] == 1) {
- echo "le livre a bien ete suprimer";
- }	
-?>
-</div>
-<div class='infos'>
-	<header>
-<h1>Gestion livre :</h1>
-	</header><br>
-	       <input  name="trie" type="submit" value="supCO">
-       	<input  name="trie" type="submit" value="modifCO">  <br>
+ echo "CA MARCHE";
+ }	else{
+ 	echo "champ isbn requie";
+ }
 
-
-
-<?php	
-if (empty($_POST['trie'])) {$tri=NULL;
-}else {$tri = htmlspecialchars($_POST['trie']);// recuperer ce que  inscrit dans barre recherche
-}
-switch ($tri) {
-	case 'modifCO';//mod CO
-		echo "modifCO";?>
-		<form name="form" action="" method="post">
-			<input  name="tri2" type="submit" value="login">
-       		<input  name="tri2" type="submit" value="mdp">
-       	</form><?php
-       	if (empty($_POST['tri2'])) {$tri2=NULL;
-		}else {$tri2 = htmlspecialchars($_POST['tri2']);// recuperer ce que  inscrit dans barre recherche
-		}
-
-		
-		?><form name="form" action="" method="post">
-			<p><input type="text" name="search" id="search" placeholder="Entrée un login">
-			<input type="submit" value="GO"></p>
-		</form><?php
-	break;
-	case 'supCo':// sup compte
-		echo "supco";
-		?><form name="form" action="" method="post">
- 			<p><input type="text" name="search" id="search" placeholder="Entrée un Login">
-			<input type="submit" value="GO"></p>
-		</form><?php
-
-		//$sql = " DELETE FROM membre WHERE login='$text';"; // requete pour trier par ordre decroisant 
-	break;
-} 
+       
+<?php
 echo"</div>";
 ?>
 </div>
