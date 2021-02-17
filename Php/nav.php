2cd88c1c5a6e6ff7dd592337c2823bf82e890d2b
Axelerati0n <76069490+Axelerati0n@users.noreply.github.com>
2021-02-17 17:32:56 +0100

    Update nav.php

--- Php/nav.php
+++ Php/nav.php
@@ -2,7 +2,7 @@
 
 <nav>
     <ul>
-      <li><a href="index.php">Menu</a></li>
+      <li><a href="<?php echo $CONFIG['root_path'] ?>index.php">Menu</a></li>
       </li>
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/auteurs.php">Auteurs</a></li>
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/compte.php">Mon compte</a></li>
