6a27826c68587369416d8e99367a25da073991a0
Axelerati0n <76069490+Axelerati0n@users.noreply.github.com>
2021-02-18 17:27:19 +0100

    Update membre.php

--- Php/membre.php
+++ Php/membre.php
@@ -6,8 +6,8 @@
    <body>
      <form method="post" action="membre.php">
 
-       Email : <input type="email" name="email" placeholder="Entrer votre Email" value="<?php if (isset($_POST['email'])) echo htmlentities(trim($_POST['email'])); ?>"><br>
-      Entrer la date d'umpreint <input type="date" name="date" placeholder="annee-moi-jour" value="<?php if (isset($_POST['date'])) echo htmlentities(trim($_POST['date'])); ?>">
+       Email : <input type="email" name="email" placeholder="Entrez votre Email" value="<?php if (isset($_POST['email'])) echo htmlentities(trim($_POST['email'])); ?>"><br>
+      Entrez la date d'emprunt <input type="date" name="date" placeholder="Année-Mois-Jour" value="<?php if (isset($_POST['date'])) echo htmlentities(trim($_POST['date'])); ?>">
                        <select name="duration" required>
                        	<?php
 $sql ="SELECT Titre FROM `livre`;";
@@ -66,8 +66,8 @@ if (isset($erreur)) echo '<br /><br />',$erreur;
    <body>
      <form method="post" action="membre.php">
 
-       Email : <input type="email" name="email" placeholder="Entrer votre Email" value="<?php if (isset($_POST['email'])) echo htmlentities(trim($_POST['email'])); ?>"><br>
-      Entrer la date d'umpreint <input type="date" name="date" placeholder="annee-moi-jour" value="<?php if (isset($_POST['date'])) echo htmlentities(trim($_POST['date'])); ?>">
+       Email : <input type="email" name="email" placeholder="Entrez votre Email" value="<?php if (isset($_POST['email'])) echo htmlentities(trim($_POST['email'])); ?>"><br>
+      Entrez la date d'emprunt <input type="date" name="date" placeholder="Année-mois-jour" value="<?php if (isset($_POST['date'])) echo htmlentities(trim($_POST['date'])); ?>">
                        <select name="duration" required>
                        	<?php
 $sql ="SELECT Titre FROM `livre`;";
@@ -109,12 +109,12 @@ if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['email'
 		$sql = "INSERT INTO Reservation (login,Date,LivreIsbn) VALUES ('login','$date','$isbn')";
 		$req = mysqli_query($link,$sql) or exit('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error($link));
 		mysqli_close ($link);
-		echo "la commande a ete prise";
+		echo "La commande a été prise";
   }else{
   	$erreur = 'Au moins un des champs est vide.';
 }
 }
-if (isset($erreur)) echo '<br /><br />',$erreur;
+if (isset($erreur)) echo '<br><br>',$erreur;
 ?>
   </body>
  </html>
