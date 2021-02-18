532ad0633b6965754d23f9d80a78153a14e58b77
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-02-18 16:42:02 +0100

    Add files via upload

--- Php/header.php
+++ Php/header.php
@@ -2,32 +2,45 @@
 <html lang="fr">
 <head>
   <?php require "Config.php"?>
+
+
   <meta charset="utf-8">
   <link rel="stylesheet" href="<?php echo $CONFIG["root_path"] ?>Css/style.css">
   <link rel="shortcut icon" type="image/png" href="<?php echo $CONFIG["root_path"] ?>img/logo.png">
   <title>StarBooks</title>
 </head>
 <body>
-<div class=navigation>	
+<div class=navigation>
 <header>
   <div class=logo>
   <p><img src="<?php echo $CONFIG["root_path"] ?>img/logo" alt="logo StarBooks" align=left class="logoo"><p>
-  <?php
-			session_start();
-			if (isset($_SESSION['login'])) {?>   <!-- si  login n'est pas null allor la persoone est connecter-->
-				<a class=bouton href="#"><img src="<?php echo $CONFIG['root_path'] ?>img/panier.png" class="conn"  align=middle>Mes réservations</a>
-				<a class=bouton href="deco.php"><img src="<?php echo $CONFIG['root_path'] ?>img/compte.png" class="conn"  align=middle>Deconnexion</a>
-				<a class=bouton href="membre.php"><img src="<?php echo $CONFIG['root_path'] ?>img/compte.png"class="conn"  align=middle>Espace membre</a><?php
-				// si le login est admin alors il s'ajit de l'admin
-					if($_SESSION['login'] == "admin"){?>
-						<a class=bouton href="admin.php"><img src="<?php echo $CONFIG["root_path"] ?>img/compte.png" class="conn" align=middle>Admin</a> <?php
-					}
-			}else{?><!--  si le login  est null alors --> 
-				<a class=bouton href="connexion.php"><img src="<?php echo $CONFIG["root_path"] ?>img/compte.png" class="conn"  align=middle>Connexion</a><?php
-			}?>
-  
+
+    <ul id="menu">
+
+    	<li class="bouton"><a href="#">Menu</a>
+
+    		<ul>
+          <div class="bouton2">
+          <?php
+        			session_start();
+        			if (isset($_SESSION['login'])) {?>   <!-- si  login n'est pas null allor la persoone est connecter-->
+
+        				<li><a class=bouton href="deco.php"><img src="<?php echo $CONFIG['root_path'] ?>img/compte.png" class="conn" align=middle><br>Deconnexion</a></li>
+        				<li><a class=bouton href="membre.php"><img src="<?php echo $CONFIG['root_path'] ?>img/compte.png" class="conn" align=middle>Espace membre</a></li><?php
+        				// si le login est admin alors il s'ajit de l'admin
+        					if($_SESSION['login'] == "admin"){?>
+        						<a class=bouton href="admin.php"><img src="<?php echo $CONFIG["root_path"] ?>img/compte.png" class="conn" align=middle><br>_____Admin_____</a> <?php
+        					}
+        			}else{?><!--  si le login  est null alors -->
+        				<a class=bouton2 href="connexion.php"><img src="<?php echo $CONFIG["root_path"] ?>img/compte.png" class="conn" align=middle><br>Connexion</a><?php
+        			}?>
+    		</ul>
+      </ul>
 </div>
+
   <?php require "nav.php"; ?>
+
+
 </header>
 </div>
 <div class="marge"></div>
