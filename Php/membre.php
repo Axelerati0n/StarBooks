50b6e7130afcae3c16a4b68c034b7e47382f4e27
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-03-06 17:12:02 +0100

    Add files via upload

--- Php/membre.php
+++ Php/membre.php
@@ -9,7 +9,7 @@
 
        Email : <input type="email" name="email" placeholder="Entrez votre Email" value="<?php if (isset($_POST['email'])) echo htmlentities(trim($_POST['email'])); ?>"><br>
       Entrez la date d'emprunt <input type="date" name="date" placeholder="Année-mois-jour" value="<?php if (isset($_POST['date'])) echo htmlentities(trim($_POST['date'])); ?>">
-                       <select name="duration" required>
+                       <select class="duration"name="duration" required>
                        	<?php
 $sql ="SELECT Titre,isbn FROM `livre`;";
 $link = mysqli_connect($HOST , $user, $password, $database);
@@ -43,7 +43,7 @@ require "config.php";
   // Vérifie qu'il provient d'un formulaire
     $_POST['login']= $_SESSION['login'];
     if (isset($_POST['submit']) && $_POST['submit'] == 'submit') {
-if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['email']) && !empty($_POST['email']))) { // req   update non   date  where  nom liv
+if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['email']) && !empty($_POST['email'])) && (isset($_POST['date']) && !empty($_POST['date']))) { // req   update non   date  where  nom liv
 		$login=$_POST['login'];
 		$date=$_POST['date'];
 		$isbn = $_POST['duration'];
@@ -61,17 +61,19 @@ if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['email'
 if (isset($erreur)) echo '<br><br>',$erreur;
 ?>
 </div><br>
-</div>
+</div><br>
 <?php
 $link = mysqli_connect($HOST , $user, $password, $database);
 if(!mysqli_set_charset($link,"utf8mb4")){//encodage  en utf8mb4
 	printf(mysqli_error($link));
 	exit();
 }
-echo "Vos Reservations : ";
+echo "<div class='grilContact'>";
+echo "<header><h1 align='center'>Vos Reservations</h1></header>";
+echo"</div>";
 echo "<div class='gril'>";
-
-$sql = "SELECT Titre,Isbn FROM reservation JOIN livre ON Livre.isbn = reservation.livreisbn ";
+$login=$_POST['login'];
+$sql = "SELECT Titre,Isbn,date FROM reservation JOIN livre ON Livre.isbn = reservation.livreisbn WHERE login LIKE '$login'";
 $req = mysqli_query($link,$sql);
 if ($req) {
 	while($data = mysqli_fetch_array($req, MYSQLI_ASSOC)){
@@ -79,6 +81,7 @@ if ($req) {
 			 echo "<div class='ca'>";
 		  		 	echo "<a href='detail.php?isbn=" . $data['Isbn'] . "'><img src='../img/Livres/" . $data["Isbn"] . ".jpg' class='img' alt=''></a><div align='center'>";
 		  		 	echo "<p class='pc'>Titre : " . $data["Titre"] . "</p>";
+            echo "<p class='pc'>date d'emprunt : " . $data["date"] . "</p>";
 					echo "</div>";
 				echo "</div>";
 		echo "</ul>";
