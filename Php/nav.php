190283ed4ccff0d0ae54334dcac9ae4bd4a130b4
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-03-06 16:12:19 +0100

    Add files via upload

--- Php/nav.php
+++ Php/nav.php
@@ -4,7 +4,7 @@
     <ul>
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/index.php">Menu</a></li>
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/auteurs.php">Auteurs</a></li>
-      <li><a href="<?php echo $CONFIG['root_path'] ?>Php/compte.php">Mon compte</a></li>
+      <li><a href="<?php echo $CONFIG['root_path'] ?>Php/membre.php">Mon compte</a></li>
       <li><a href="<?php echo $CONFIG['root_path'] ?>Php/contact.php">Nous Contacter</a></li>
     </ul>
   </nav>
