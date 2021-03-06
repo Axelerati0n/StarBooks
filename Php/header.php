f002e3f9be52b4193fb4fa18bd845c622e02b8d2
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-03-06 17:12:51 +0100

    Add files via upload

--- Php/header.php
+++ Php/header.php
@@ -13,7 +13,7 @@
 <div class=navigation>
 <header>
   <div class=logo>
-  <p><img src="<?php echo $CONFIG["root_path"] ?>img/logo" alt="logo StarBooks" align=left class="logoo"><p>
+  <a href="<?php echo $CONFIG["root_path"] ?>Php/index.php"><img src="<?php echo $CONFIG["root_path"] ?>img/logo" alt="logo StarBooks" align=left class="logoo"></a>
 
     <ul id="menu">
 
@@ -25,11 +25,11 @@
         			session_start();
         			if (isset($_SESSION['login'])) {?>   <!-- si  login n'est pas null allor la persoone est connecter-->
 
-        				<li><a class=bouton href="deco.php"><img src="<?php echo $CONFIG['root_path'] ?>img/deco.png" class="conn" align=middle>Deconnexion</a></li>
-        				<li><a class=bouton href="membre.php"><img src="<?php echo $CONFIG['root_path'] ?>img/compte.png" class="conn" align=middle>Espace membre</a></li><?php
+        				<li><a class=bouton href="<?php echo $CONFIG['root_path'] ?>Php/deco.php"><img src="<?php echo $CONFIG['root_path'] ?>img/deco.png" class="conn" align=middle>Deconnexion</a></li>
+        				<li><a class=bouton href="<?php echo $CONFIG['root_path'] ?>Php/membre.php"><img src="<?php echo $CONFIG['root_path'] ?>img/compte.png" class="conn" align=middle>Espace membre</a></li><?php
         				// si le login est admin alors il s'ajit de l'admin
         					if($_SESSION['login'] == "admin"){?>
-        						<a class=bouton href="admin.php"><img src="<?php echo $CONFIG["root_path"] ?>img/admin.png" class="conn" align=middle>__Admin__</a> <?php
+        						<a class=bouton href="<?php echo $CONFIG['root_path'] ?>Php/admin.php"><img src="<?php echo $CONFIG["root_path"] ?>img/admin.png" class="conn" align=middle>__Admin__</a> <?php
         					}
         			}else{?><!--  si le login  est null alors -->
         				<a class=bouton href="connexion.php"><img src="<?php echo $CONFIG["root_path"] ?>img/compte.png" class="conn" align=middle><br>Connexion</a><?php
